73c6eff4e81327c51137a3cd5125d2966d8af05a
manticore-siva <43838423+manticore-siva@users.noreply.github.com>
2020-02-10 18:55:46 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,25 +4,25 @@
 //                                 //
 /////////////////////////////////////
 
-package com.siva;
+package com.siva;                           /// package name
+    
+import java.util.Scanner;                   /// import Scanner class
 
-import java.util.Scanner;
+public class Main {                         /// start here class
 
-public class Main {
-
-    public static void main(String[] args) {
+    public static void main(String[] args) {        /// start here method
         int x,y;
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);         /// by used to get a input values to user
         System.out.println("Enter ethe x values");
-        x = input.nextInt();
+        x = input.nextInt();                            /// get a x value
         System.out.println("Enter the y value");
-        y = input.nextInt();
-        System.out.println("After Swape values");
-        y = x+y;
-        x = y-x;
-        y= y-x;
-        System.out.println(x);
-        System.out.println(y);
+        y = input.nextInt();                            /// get a y value
+        System.out.println("After Swap values");
+        y = x+y;                                       //// add "x" an "y" values and store to "y"
+        x = y-x;                                       //// subtrc "y"an "x" value after store to "y" now this variable have "x" value
+        y= y-x;                                        //// subtrc "y"an "x" value after store to "x" now this variable have "y" value
+        System.out.println("x value :"+x);
+        System.out.println("y value :"+y);                      /// finaly print swap two variable value
 
     }
 }
